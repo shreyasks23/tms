ec485d5eda6d073cc6f7e24bdb9e612d89a9c658
Shreyas, K S <KS.Shreyas@philips.com>
2025-12-05 14:00:26 +0530

    styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,77 @@
-import { Container, Typography, Box } from "@mui/material";
+import {
+  Container,
+  Typography,
+  Box,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Button,
+  ButtonGroup,
+} from "@mui/material";
 import { QnAPage } from "./pages/QnAPage";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage";
 import { DashboardPage } from "./pages/DashboardPage";
 
-const darkTheme = createTheme({
+const theme = createTheme({
   palette: {
-    mode: "light",
+    background: {
+      paper: '#fff',
+    },
+    text: {
+      primary: '#173A5E',
+      secondary: '#46505A',
+    },
+    action: {
+      active: '#001E3C',
+    },
+    success: {
+      main: '#4CAF50',
+      dark: '#009688',
+    },
   },
 });
 
 function App() {
+  const navigate = useNavigate();
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter>
-        <Container maxWidth="sm">
-          <Box mt={4}>
-            <Typography variant="h4" gutterBottom>
-              Quiz App
-            </Typography>
-            <nav className="navbar">
-              <Link to="/login">Login</Link>
-              <Link to="/dashboard">Dashboard</Link>
-              <Link to="/test">Test</Link>
-            </nav>
-          </Box>
-        </Container>
+      <Container fixed>
+          <AppBar color="transparent" position="static">
+            <Toolbar>
+              <IconButton></IconButton>
+              <Typography variant="h4" gutterBottom>
+                Quiz App
+              </Typography>
+              <Box
+                sx={{
+                  flexGrow: 1,
+                  justifyContent: "flex-end",
+                  display: "flex",
+                }}
+              >
+                <ButtonGroup>
+                  <Button onClick={() => navigate("/login")}>Login</Button>
+
+                  <Button onClick={() => navigate("/dashboard")}>Dashboard</Button>
+
+                  <Button onClick={() => navigate("/test")}>Test</Button>
+                </ButtonGroup>
+              </Box>
+            </Toolbar>
+          </AppBar>
+        <Box mt={4}>
         <Routes>
           <Route path="login" element={<LoginPage />} />
           <Route path="dashboard" element={<DashboardPage />} />
           <Route path="test" element={<QnAPage />} />
         </Routes>
-      </BrowserRouter>
+        </Box>
+      </Container>
     </ThemeProvider>
   );
 }

5b28f0d6921205ade16b96d1d8d3ef6303a0b450
Shreyas KS <shreyasks23@gmail.com>
2025-12-06 13:34:51 +0530

    backend schema changes

--- server/src/models/Question.ts
+++ server/src/models/Question.ts
@@ -1,16 +1,26 @@
 import mongoose, { Schema, Document } from "mongoose";
 
+export interface IOption {
+  id: number;
+  text: string;
+  isCorrect: boolean;
+}
+
 export interface IQuestion extends Document {
   text: string;
-  options: string[];
-  correctIndex: number;
+  options: IOption[];
   description?: string;
 }
 
+const OptionSchema: Schema = new Schema<IOption>({
+  id: { type: Number, required: true },
+  text: { type: String, required: true },
+  isCorrect: { type: Boolean, required: true },
+});
+
 const QuestionSchema: Schema = new Schema<IQuestion>({
   text: { type: String, required: true },
-  options: { type: [String], required: true },
-  correctIndex: { type: Number, required: true },
+  options: { type: [OptionSchema], required: true },
   description: { type: String, required: false },
 });
 
